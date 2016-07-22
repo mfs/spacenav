705b3bc4b97b6b603935d2c3f6ac974909ec21f8
Mike Sampson <mike@sambodata.com>
2016-07-22 22:04:30 +1000

    Initial documentation import.

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["spacenav"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='spacenav/enum.Event.html' title='spacenav::Event'>Event</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
